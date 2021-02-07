ea478d6f6c15cb04703a51bd364705ead9f668d0
Neradoc <neraOnGit@ri1.fr>
2021-02-07 02:54:46 +0100

    remove file

--- wsdemo.py
+++
@@ -1,6 +0,0 @@
-import wifi
-from secrets import secrets
-wifi.radio.connect(ssid=secrets["ssid"], password=secrets["password"])
-
-#import examples.echo_websocket_org
-import examples.echo_ssl_websocket_org
